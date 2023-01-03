aa5597a9a891989e2fa72b589bf63cf7bf369cb1
Yusan Kurban <21994267+yusank@users.noreply.github.com>
2023-01-03 11:33:21 +0800

    Feature/field selector (#21)
    * feat: support field selector & field match expressions

---
+++ apis/policy/v1alpha1/selector_types.go
@@ -0,0 +1,123 @@
+/*
+Copyright 2022 by k-cloud-labs org.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+import (
+	"fmt"
+	"strings"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
+
+type FieldSelector struct {
+	// matchFields is a map of {key,value} pairs. A single {key,value} in the matchFields
+	// map is equivalent to an element of matchExpressions, whose key field is "key", the
+	// operator is "In", and the values array contains only "value".
+	// +optional
+	MatchFields map[string]string `json:"matchFields,omitempty"`
+	// matchExpressions is a list of fields selector requirements. The requirements are ANDed.
+	// +optional
+	MatchExpressions []*FieldSelectorRequirement `json:"matchExpressions,omitempty"`
+}
+
+type FieldSelectorRequirement struct {
+	// Field is the field key that the selector applies to.
+	// Must provide whole path of key, such as `metadata.annotations.uid`
+	Field string `json:"field"`
+	// operator represents a key's relationship to a set of values.
+	// Valid operators are In, NotIn, Exists and DoesNotExist.
+	Operator metav1.LabelSelectorOperator `json:"operator"`
+	// values is an array of string values. If the operator is In or NotIn,
+	// the values array must be non-empty. If the operator is Exists or DoesNotExist,
+	// the values array must be empty.
+	// +optional
+	Value []string `json:"value,omitempty"`
+}
+
+func (r *FieldSelectorRequirement) MatchObject(obj *unstructured.Unstructured) (bool, error) {
+	v, found, err := unstructured.NestedString(obj.Object, strings.Split(r.Field, ".")...)
+	if err != nil {
+		return false, err
+	}
+
+	switch r.Operator {
+	case metav1.LabelSelectorOpExists:
+		return found, nil
+	case metav1.LabelSelectorOpDoesNotExist:
+		return !found, nil
+	case metav1.LabelSelectorOpIn:
+		if !found {
+			return found, nil
+		}
+
+		for _, s := range r.Value {
+			if s == v {
+				return true, nil
+			}
+		}
+
+		return false, nil
+	case metav1.LabelSelectorOpNotIn:
+		var in bool
+		for _, s := range r.Value {
+			if s == v {
+				in = true
+				break
+			}
+		}
+
+		return !in, nil
+	default:
+		return false, fmt.Errorf("unknown operator:%v", r.Operator)
+	}
+}
+
+func (f *FieldSelector) MatchObject(obj *unstructured.Unstructured) (bool, error) {
+	for k, v := range f.MatchFields {
+		match, err := matchObj(obj, k, v)
+		if err != nil {
+			return false, err
+		}
+
+		if !match {
+			return match, nil
+		}
+	}
+
+	for i := range f.MatchExpressions {
+		match, err := f.MatchExpressions[i].MatchObject(obj)
+		if err != nil {
+			return false, err
+		}
+
+		if !match {
+			return match, nil
+		}
+	}
+
+	return true, nil
+}
+
+func matchObj(obj *unstructured.Unstructured, field, value string) (bool, error) {
+	v, found, err := unstructured.NestedString(obj.Object, strings.Split(field, ".")...)
+	if err != nil {
+		return false, err
+	}
+
+	return found && value == v, nil
+}
---
+++ utils/selector_test.go
@@ -0,0 +1,356 @@
+package utils
+
+import (
+	"testing"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+
+	policyv1alpha1 "github.com/k-cloud-labs/pkg/apis/policy/v1alpha1"
+)
+
+func newObjectForSelector() *unstructured.Unstructured {
+	return &unstructured.Unstructured{
+		Object: map[string]interface{}{
+			"apiVersion": "v1",
+			"kind":       "Pod",
+			"metadata": map[string]interface{}{
+				"name":      "pod1",
+				"namespace": "default",
+				"labels": map[string]interface{}{
+					"l1": "v1",
+					"l2": "v2",
+				},
+				"annotations": map[string]interface{}{
+					"a1": "v1",
+					"a2": "v2",
+				},
+			},
+			"spec": map[string]interface{}{
+				"field1": "v1",
+			},
+			"status": map[string]interface{}{
+				"field1": "v1",
+			},
+		},
+	}
+}
+
+func TestResourceMatchSelectors(t *testing.T) {
+	type args struct {
+		resource  *unstructured.Unstructured
+		selectors []policyv1alpha1.ResourceSelector
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		{
+			name: "name",
+			args: args{
+				resource: newObjectForSelector(),
+				selectors: []policyv1alpha1.ResourceSelector{
+					{
+						APIVersion: "v1",
+						Kind:       "Pod",
+						Namespace:  "default",
+						Name:       "pod1",
+					},
+				},
+			},
+			want: true,
+		},
+		{
+			name: "labels1",
+			args: args{
+				resource: newObjectForSelector(),
+				selectors: []policyv1alpha1.ResourceSelector{
+					{
+						APIVersion: "v1",
+						Kind:       "Pod",
+						LabelSelector: &metav1.LabelSelector{
+							MatchLabels: map[string]string{
+								"l1": "v1",
+								"l2": "v2",
+							},
+						},
+					},
+				},
+			},
+			want: true,
+		},
+		{
+			name: "labels2",
+			args: args{
+				resource: newObjectForSelector(),
+				selectors: []policyv1alpha1.ResourceSelector{
+					{
+						APIVersion: "v1",
+						Kind:       "Pod",
+						LabelSelector: &metav1.LabelSelector{
+							MatchExpressions: []metav1.LabelSelectorRequirement{
+								{
+									Key:      "l1",
+									Operator: metav1.LabelSelectorOpExists,
+								},
+							},
+						},
+					},
+				},
+			},
+			want: true,
+		},
+		{
+			name: "labels3",
+			args: args{
+				resource: newObjectForSelector(),
+				selectors: []policyv1alpha1.ResourceSelector{
+					{
+						APIVersion: "v1",
+						Kind:       "Pod",
+						LabelSelector: &metav1.LabelSelector{
+							MatchLabels: map[string]string{
+								"l1": "v1",
+								"l2": "v2",
+								"l3": "v3",
+							},
+						},
+					},
+				},
+			},
+			want: false,
+		},
+		{
+			name: "field1",
+			args: args{
+				resource: newObjectForSelector(),
+				selectors: []policyv1alpha1.ResourceSelector{
+					{
+						APIVersion: "v1",
+						Kind:       "Pod",
+						FieldSelector: &policyv1alpha1.FieldSelector{
+							MatchFields: map[string]string{
+								"spec.field1": "v1",
+							},
+						},
+					},
+				},
+			},
+			want: true,
+		},
+		{
+			name: "field2",
+			args: args{
+				resource: newObjectForSelector(),
+				selectors: []policyv1alpha1.ResourceSelector{
+					{
+						APIVersion: "v1",
+						Kind:       "Pod",
+						FieldSelector: &policyv1alpha1.FieldSelector{
+							MatchFields: map[string]string{
+								"spec.field1": "v1", // valid
+								"spec.field2": "v2", // invalid
+							},
+						},
+					},
+				},
+			},
+			want: false,
+		},
+		{
+			name: "fieldAnnotation",
+			args: args{
+				resource: newObjectForSelector(),
+				selectors: []policyv1alpha1.ResourceSelector{
+					{
+						APIVersion: "v1",
+						Kind:       "Pod",
+						FieldSelector: &policyv1alpha1.FieldSelector{
+							MatchFields: map[string]string{
+								"metadata.annotations.a1": "v1",
+							},
+						},
+					},
+				},
+			},
+			want: true,
+		},
+		{
+			name: "fieldExpressionExist",
+			args: args{
+				resource: newObjectForSelector(),
+				selectors: []policyv1alpha1.ResourceSelector{
+					{
+						APIVersion: "v1",
+						Kind:       "Pod",
+						FieldSelector: &policyv1alpha1.FieldSelector{
+							MatchExpressions: []*policyv1alpha1.FieldSelectorRequirement{
+								{
+									Field:    "metadata.annotations.a1",
+									Operator: metav1.LabelSelectorOpExists,
+								},
+							},
+						},
+					},
+				},
+			},
+			want: true,
+		},
+		{
+			name: "fieldExpressionNotExist",
+			args: args{
+				resource: newObjectForSelector(),
+				selectors: []policyv1alpha1.ResourceSelector{
+					{
+						APIVersion: "v1",
+						Kind:       "Pod",
+						FieldSelector: &policyv1alpha1.FieldSelector{
+							MatchExpressions: []*policyv1alpha1.FieldSelectorRequirement{
+								{
+									Field:    "metadata.annotations.aaa",
+									Operator: metav1.LabelSelectorOpDoesNotExist,
+								},
+							},
+						},
+					},
+				},
+			},
+			want: true,
+		},
+		{
+			name: "fieldExpressionIn",
+			args: args{
+				resource: newObjectForSelector(),
+				selectors: []policyv1alpha1.ResourceSelector{
+					{
+						APIVersion: "v1",
+						Kind:       "Pod",
+						FieldSelector: &policyv1alpha1.FieldSelector{
+							MatchExpressions: []*policyv1alpha1.FieldSelectorRequirement{
+								{
+									Field:    "metadata.annotations.a1",
+									Operator: metav1.LabelSelectorOpIn,
+									Value:    []string{"v1", "v2", "v3"},
+								},
+							},
+						},
+					},
+				},
+			},
+			want: true,
+		},
+		{
+			name: "fieldExpressionNotIn",
+			args: args{
+				resource: newObjectForSelector(),
+				selectors: []policyv1alpha1.ResourceSelector{
+					{
+						APIVersion: "v1",
+						Kind:       "Pod",
+						FieldSelector: &policyv1alpha1.FieldSelector{
+							MatchExpressions: []*policyv1alpha1.FieldSelectorRequirement{
+								{
+									Field:    "metadata.annotations.a1",
+									Operator: metav1.LabelSelectorOpNotIn,
+									Value:    []string{"v4", "v5", "v6"},
+								},
+							},
+						},
+					},
+				},
+			},
+			want: true,
+		},
+		{
+			name: "fieldExpressionInvalid",
+			args: args{
+				resource: newObjectForSelector(),
+				selectors: []policyv1alpha1.ResourceSelector{
+					{
+						APIVersion: "v1",
+						Kind:       "Pod",
+						FieldSelector: &policyv1alpha1.FieldSelector{
+							MatchExpressions: []*policyv1alpha1.FieldSelectorRequirement{
+								{
+									Field:    "metadata.annotations.a1",
+									Operator: "!=", // not support
+									Value:    []string{"v4"},
+								},
+							},
+						},
+					},
+				},
+			},
+			want: false,
+		},
+		{
+			name: "labelsAndField1",
+			args: args{
+				resource: newObjectForSelector(),
+				selectors: []policyv1alpha1.ResourceSelector{
+					{
+						APIVersion: "v1",
+						Kind:       "Pod",
+						LabelSelector: &metav1.LabelSelector{
+							MatchLabels: map[string]string{
+								"l1": "v1",
+								"l2": "v2",
+							},
+						},
+						FieldSelector: &policyv1alpha1.FieldSelector{
+							MatchFields: map[string]string{
+								"status.field1": "v1",
+							},
+						},
+					},
+				},
+			},
+			want: true,
+		},
+		{
+			name: "labelsAndField2",
+			args: args{
+				resource: newObjectForSelector(),
+				selectors: []policyv1alpha1.ResourceSelector{
+					{
+						APIVersion: "v1",
+						Kind:       "Pod",
+						LabelSelector: &metav1.LabelSelector{
+							MatchLabels: map[string]string{
+								"l1": "v1",
+								"l2": "v2",
+							}, // valid
+						},
+						FieldSelector: &policyv1alpha1.FieldSelector{
+							MatchFields: map[string]string{
+								"metadata.annotations.field1": "v1", // invalid
+							},
+						},
+					},
+				},
+			},
+			want: false,
+		},
+		{
+			name: "all",
+			args: args{
+				resource: newObjectForSelector(),
+				selectors: []policyv1alpha1.ResourceSelector{
+					{
+						APIVersion: "v1",
+						Kind:       "Pod",
+					},
+				},
+			},
+			want: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := ResourceMatchSelectors(tt.args.resource, tt.args.selectors...); got != tt.want {
+				t.Errorf("ResourceMatchSelectors() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
